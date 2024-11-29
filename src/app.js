59883bea62bb7218453e5112a7188d11b3062a5f
kim nan seul <154604350+seulnan@users.noreply.github.com>
2024-11-30 04:32:26 +0900

    [REFACTOR] app.js수정
    - 중복로직 수정
    - 반복적인 라우트설정제거
    - 기본 라우트 활성화
    - bodyParser를 최신 express.json()으로 대체

--- src/app.js
+++ src/app.js
@@ -3,25 +3,32 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/mongodb');
 
+
 const app = express();
 
 // Database 연결
 connectDB();
 
+
 // Middleware 설정
-app.use(cors());
-app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: 'http://localhost:3000', // 허용할 클라이언트 URL
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], // 허용할 HTTP 메서드
+    credentials: true, // 쿠키, 인증 정보 허용 여부
+  })
+);
+app.use(express.json()); // 최신 Express에서 body-parser 대체
+app.use(bodyParser.urlencoded({ extended: true })); // URL 인코딩된 데이터 파싱
 
 // 라우트 설정
-app.use('/api/budgets', require('./routes/budgetRoutes'));
-app.use('/api/transactions', require('./routes/transactionRoutes'));
-app.use('/api/pots', require('./routes/potRoutes'));
-app.use('/api/recurrings', require('./routes/recurringRoutes'));
+const routes = ['budget', 'transaction', 'pot', 'recurring'];
+routes.forEach((route) => {
+  app.use(`/api/${route}`, require(`./routes/${route}Routes`));
+});
 
-
-// // 기본 라우트
-// app.get('/', (req, res) => {
-//   res.send('Family Budget API is running.');
-// });
+app.get('/', (req, res) => {
+  res.send('Family Budget API is running.');
+});
 
 module.exports = app;
